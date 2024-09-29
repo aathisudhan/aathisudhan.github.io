9b500860ce9f8ffb9fd95c520a263ec9fe465390
aathisudhan <aathisudhan.ai@gmail.com>
2024-09-29 12:53:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Redirect after a short delay (in milliseconds)
+setTimeout(() => {
+    window.location.href = 'home.html';
+}, 3000); // Change this value to set how long to wait before redirecting (3000ms = 3 seconds)
